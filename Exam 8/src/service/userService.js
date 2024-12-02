49b15dfd788e32d0efd48142d6b1885ffa386cfc
Radoslav <todorovradoslav6@gmail.com>
2024-12-02 08:07:37 +0200

    Make Exam 8

---
+++ Exam 8/src/service/userService.js
@@ -0,0 +1,24 @@
+export function setUserData(id,accessToken){
+    localStorage.setItem('userId',id);
+    localStorage.setItem('accessToken',accessToken)
+}
+
+
+export function getAccessToken(){
+    return localStorage.getItem('accessToken');
+}
+
+
+export function getUserId(){
+    return localStorage.getItem('userId');
+}
+
+
+export function clearUserData(){
+    localStorage.clear();
+}
+
+
+export function isOwner(userId,ownerId){
+    return userId === ownerId;
+}
